7028b9311e6c33bb2c570050f8145baca51179ca
roche <email": null>
2016-02-21 10:54:54 -0800

    More MVC info

--- connect_four_controller.js
+++ connect_four_controller.js
@@ -4,35 +4,15 @@ $(document).ready(function(){
   var view = new View();
   var play = new Play();
 
+  view.StartGame =
+  play.who's the player
+  view.get click
+  board.addchecker
+  board.talkto View
+  view.drop checker
+  play.swtich player
 
 
 
-  $(".checker00").click(function(){
-    $(".checker00").animate({top: "+=240px"});
-  });
-
-  $(".checker01").click(function(){
-    $(".checker01").animate({top: "+=600px"});
-  });
-
-  $(".checker02").click(function(){
-    $(".checker02").animate({top: "+=120px"});
-  });
-
-  $(".checker03").click(function(){
-    $(".checker03").animate({top: "+=480px"});
-  });
-
-  $(".checker04").click(function(){
-    $(".checker04").animate({top: "+=360px"});
-  });
-
-  $(".checker05").click(function(){
-    $(".checker05").animate({top: "+=120px"});
-  });
-
-  $(".checker06").click(function(){
-    $(".checker06").animate({top: "+=600px"});
-  });
 
 });
--- connect_four_model.js
+++ connect_four_model.js
@@ -8,12 +8,12 @@ function Board(){
   this.col6 = [];
 }
 
-Board.prototype.horizontalSolve =
-Board.prototype.verticalSolve =
-Board.prototype.backSolve =
-Board.prototype.forwardSolve =
-Board.prototype.addChecker =
-Board.prototype.talkToView = //sends coordinates col# and row #
+Board.prototype.horizontalSolve = nil
+Board.prototype.verticalSolve = nil
+Board.prototype.backSolve = nil
+Board.prototype.forwardSolve = nil
+Board.prototype.addChecker = nil
+Board.prototype.talkToView = nil//sends coordinates col# and row #
 
 function Play(){
   this.color = ???
