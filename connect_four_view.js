6506826a4a67f9e85e689e01d67ad59e4963250a
roche <email": null>
2016-02-21 16:20:05 -0800

    Swith players every round

--- connect_four_view.js
+++ connect_four_view.js
@@ -2,59 +2,53 @@
 function View(){};
 
 // checker click function starts here
-View.prototype.getClick = function(color, board, dropChecker){
+View.prototype.getClick = function(board, play, dropChecker){
     $(".checker").click(function(e){
       e.preventDefault;
       console.log(this);
       var col = $(this).attr('col');
-      dropChecker(color, board.addChecker(color, col));
+      dropChecker(play, board.addChecker(play.color, col));
     });
 
 }
 // checker click function ends here
 
-var dropChecker = function (color, coordinates){
+var dropChecker = function (play, coordinates){
   var checkerRow = coordinates[1];
   var checkerColumn = coordinates[0];
   var checkerColumnJquery = ".checker" + checkerColumn;
   var destinationCell = '.cell' + checkerColumn + checkerRow
   var checkerRowJquery = (6 - checkerRow) * 120;
-    console.log(checkerRow + " " + checkerColumn);
-    $(checkerColumnJquery).css("background-color", color);
-    $(checkerColumnJquery).animate({top: "+=" + checkerRowJquery + "px"}, 1000);
-    setTimeout(function(){
-      $(destinationCell).css('background-color', color);
-    }, 1000);
-    $(checkerColumnJquery).animate({top: "-=" + checkerRowJquery + "px"});
-    
+    // console.log(checkerRow + " " + checkerColumn);
+  // $(checkerColumnJquery).css("background-color", play.color);
+  $(checkerColumnJquery).animate({top: "+=" + checkerRowJquery + "px"}, 1000);
+  setTimeout(function(){
+    $(destinationCell).css('background-color', play.color);
+  }, 1000);
+  $(checkerColumnJquery).animate({top: "-=" + checkerRowJquery + "px"});
+  play.switchPlayer();
 }
 
 $(document).ready(function(){
 
   // mouseover function starts here
   $(".checker").mouseover(function(){
-    
   });
 
   $(".checker").mouseout(function(){
-    
   });
   // mouseover function ends here
- 
 
   var board = new Board();
   var play = new Play();
   var view = new View();
-  var color = play.color;
   var solved = false;
 
-  
-  view.getClick(color, board, dropChecker);
-  console.log(color);
+  view.getClick(board, play, dropChecker);
 
 
 
-  
+
 
 
 
