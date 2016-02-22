a4957ef3b2209945de1b9b9737e22fcbffa7540f
Timur Catakli <timurcatakli@gmail.com>
2016-02-21 16:03:20 -0800

    Add board timer functionality

--- connect_four_view.js
+++ connect_four_view.js
@@ -1,12 +1,3 @@
-  // mouseover function starts here
-  $(".checker").mouseover(function(){
-    $(this).css('background-color', 'blue')
-  });
-
-  $(".checker").mouseout(function(){
-    $(this).css('background-color', 'gray')
-  });
-  // mouseover function ends here
 
 function View(){};
 
@@ -14,9 +5,11 @@ function View(){};
 View.prototype.getClick = function(color, board, dropChecker){
     $(".checker").click(function(e){
       e.preventDefault;
+      console.log(this);
       var col = $(this).attr('col');
       dropChecker(color, board.addChecker(color, col));
     });
+
 }
 // checker click function ends here
 
@@ -24,18 +17,44 @@ var dropChecker = function (color, coordinates){
   var checkerRow = coordinates[1];
   var checkerColumn = coordinates[0];
   var checkerColumnJquery = ".checker" + checkerColumn;
+  var destinationCell = '.cell' + checkerColumn + checkerRow
   var checkerRowJquery = (6 - checkerRow) * 120;
-
+    console.log(checkerRow + " " + checkerColumn);
     $(checkerColumnJquery).css("background-color", color);
-    $(checkerColumnJquery).animate({top: "+=" + checkerRowJquery + "px"});
+    $(checkerColumnJquery).animate({top: "+=" + checkerRowJquery + "px"}, 1000);
+    setTimeout(function(){
+      $(destinationCell).css('background-color', color);
+    }, 1000);
+    $(checkerColumnJquery).animate({top: "-=" + checkerRowJquery + "px"});
+    
 }
 
 $(document).ready(function(){
+
+  // mouseover function starts here
+  $(".checker").mouseover(function(){
+    
+  });
+
+  $(".checker").mouseout(function(){
+    
+  });
+  // mouseover function ends here
+ 
+
   var board = new Board();
   var play = new Play();
   var view = new View();
   var color = play.color;
+  var solved = false;
+
+  
   view.getClick(color, board, dropChecker);
+  console.log(color);
+
+
+
+  
 
 
 
@@ -43,16 +62,22 @@ $(document).ready(function(){
 
 
 
-  var fiveSeconds = new Date().getTime() + 5000;
 
-  $('#clock').countdown(fiveSeconds).on('update.countdown', function(event) {
 
-    var $this = $(this);
-      $this.html(event.strftime('To end: <span>%H:%M:%S</span>'));
-  }).on('finish.countdown', function(e){
-    alert("Test");
-    this.countdown('start');
-  });
+
+
+
+
+  // var fiveSeconds = new Date().getTime() + 5000;
+
+  // $('#clock').countdown(fiveSeconds).on('update.countdown', function(event) {
+
+  //   var $this = $(this);
+  //     $this.html(event.strftime('To end: <span>%H:%M:%S</span>'));
+  // }).on('finish.countdown', function(e){
+  //   alert("Test");
+  //   this.countdown('start');
+  // });
 
 
 
